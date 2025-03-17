e87e1688bf01629fa215a6712eba88a37499e186
Cem Tanrikut <cemtanrikut@gmail.com>
2025-03-17 03:29:48 +0100

    added workers routes

---
+++ backend/routes/workers.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"ticket-management-system/controllers"
+	"ticket-management-system/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+// RegisterWorkerRoutes çalışan işlemleri için route'ları ekler
+func RegisterWorkerRoutes(router *mux.Router) {
+	workerRouter := router.PathPrefix("/workers").Subrouter()
+	workerRouter.Use(middleware.AuthMiddleware)
+
+	workerRouter.HandleFunc("", controllers.GetWorkers).Methods("GET")
+	workerRouter.HandleFunc("/{id}", controllers.GetWorkerByID).Methods("GET")
+	workerRouter.HandleFunc("", controllers.CreateWorker).Methods("POST")
+	workerRouter.HandleFunc("/{id}", controllers.UpdateWorker).Methods("PUT")
+	workerRouter.HandleFunc("/{id}", controllers.DeleteWorker).Methods("DELETE")
+}
