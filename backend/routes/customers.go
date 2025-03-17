e3e8373be71c84d3d94618bd7c9e18e2e4eb9c09
Cem Tanrikut <cemtanrikut@gmail.com>
2025-03-17 03:28:54 +0100

    added customers routes

---
+++ backend/routes/customers.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"ticket-management-system/controllers"
+	"ticket-management-system/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+// RegisterCustomerRoutes müşteri işlemleri için route'ları ekler
+func RegisterCustomerRoutes(router *mux.Router) {
+	customerRouter := router.PathPrefix("/customers").Subrouter()
+	customerRouter.Use(middleware.AuthMiddleware)
+
+	customerRouter.HandleFunc("", controllers.GetCustomers).Methods("GET")
+	customerRouter.HandleFunc("/{id}", controllers.GetCustomerByID).Methods("GET")
+	customerRouter.HandleFunc("", controllers.CreateCustomer).Methods("POST")
+	customerRouter.HandleFunc("/{id}", controllers.UpdateCustomer).Methods("PUT")
+	customerRouter.HandleFunc("/{id}", controllers.DeleteCustomer).Methods("DELETE")
+}
